50da3e53536b6b7a0c82a205199470fa0a44447a
mathyoumore <mmohr@covermymeds.com>
2017-04-24 22:56:52 -0400

    scaffolding

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :personal_records
+  resources :workouts
+  resources :set_of_exercises
+  resources :exercises
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
